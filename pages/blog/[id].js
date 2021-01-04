e52468e9dcc0e2ae8105d738bd1871ec6d88107c
Shofiqul Islam Miraz <47095132+ShofiqulMiraz@users.noreply.github.com>
2021-01-04 10:08:28 +0600

    added getstaticpath on dynamic blogs

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,27 +1,47 @@
 import Head from "next/head";
-import { Card } from "antd";
+import { useRouter } from "next/router";
 
 export default function Home({ blog }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <Head>
         <title>{blog.title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Card title={blog.title} style={{ width: 300 }}>
+      <div>
+        <h1> {blog.title} </h1>
         <p>{blog.description}</p>
         by <p> {blog.author} </p>
-      </Card>
+      </div>
     </div>
   );
 }
 
-export async function getServerSideProps({ params }) {
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: "5ff12fd84a7b60228019938f" } },
+      { params: { id: "5ff0aa0f24725e00179b3f97" } },
+    ],
+
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
   const res = await fetch(
     `https://strapi-mongodb-blog.herokuapp.com/blogs/${params.id}`
   );
   const blog = await res.json();
+
   return {
     props: { blog },
+    revalidate: 1,
   };
 }
