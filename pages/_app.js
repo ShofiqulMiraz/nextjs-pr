0b2b3e95874574fb452894fa4038bc6c0754f057
Shofiqul Islam Miraz <47095132+ShofiqulMiraz@users.noreply.github.com>
2021-01-05 10:15:35 +0600

    added loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import NavBar from "../components/navbar/nav";
 import "../styles/globals.css";
 import "antd/dist/antd.css";
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
+
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home({ blog }) {
+  const ImageArray = blog.image;
+  const imagearrayurl = ImageArray.map((image) => image.url);
   return (
     <div>
       <Head>
@@ -12,18 +14,15 @@ export default function Home({ blog }) {
         <h1> {blog.title} </h1>
         <p>{blog.description}</p>
         by <p> {blog.author} </p>
-        <Image
-          src={blog.image[0].url}
-          alt="Picture of the author"
-          width={500}
-          height={500}
-        />
-        <Image
-          src={blog.image[1].url}
-          alt="Picture of the author"
-          width={500}
-          height={500}
-        />
+        {imagearrayurl.map((url, index) => (
+          <Image
+            key={index}
+            src={url}
+            alt="Picture of the author"
+            width={500}
+            height={500}
+          />
+        ))}
       </div>
     </div>
   );
